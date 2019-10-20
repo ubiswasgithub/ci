b40a02b9757da8da330468758cb18cd3d35e690c
Uttam <uttamcsedu@gmail.com>
2019-10-20 15:20:47 +0600

    change test file

--- junittest.java
+++ junittest.java
@@ -10,7 +10,7 @@ public class junittest extends TestCase {
 		Employee employee1  = new Employee(30, "JOB1", 1250);
 		Employee employee2  = new Employee(40, "JOB2", 1000);
 		
-		employee1.setEmployee_name("EMP2");
+		employee1.setEmployee_name("EMP1");
 		
 		assertEquals("EMP1", employee1.getEmployee_name());
 	}
